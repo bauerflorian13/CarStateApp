115f561ee8e4b417f82b0cc6691ad0c42ab5caa6
Florian Bauer <Bauerflorian13@gmail.com>
2017-08-16 19:26:50 +0200

    Add some basic stuff for the app
    This app will run with the TornadoFX Framework which is the same as
    JavaFX but for kotlin.

---
+++ src/main/kotlin/com/example/demo/app/MyApp.kt
@@ -0,0 +1,11 @@
+package com.example.demo.app
+
+import com.example.demo.view.MainView
+import javafx.application.Application
+import tornadofx.App
+
+class CarStateApp: App(MainView::class, Styles::class)
+
+fun main(args: Array<String>){
+    Application.launch(CarStateApp::class.java, *args)
+}
